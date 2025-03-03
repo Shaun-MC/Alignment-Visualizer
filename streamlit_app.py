fa3a83b7ede2604a1e13c64c00c9d55256c5c4be
Shaun-MC <shaunmcushman@gmail.com>
2025-03-03 11:01:02 -0800

    using sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,13 @@ st.title("Streamlit Playground")
 
 st.write("Example Widgets")
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
