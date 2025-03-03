4238f4c91742ca88fd6a330d40830f13e16c1c1c
Shaun-MC <shaunmcushman@gmail.com>
2025-03-03 09:58:16 -0800

    testing magic commands

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.title("2D Alignment Visualizer")
 st.write("Hello World")
 
-import streamlit as st
+
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
+
 
 if 'button' not in st.session_state:
     st.session_state.button = False
@@ -16,6 +25,6 @@ st.button('Click me', on_click=click_button)
 if st.session_state.button:
     # The message and nested widget will remain on the page
     st.write('Button is on!')
-    st.slider('Select a value')
+    
 else:
     st.write('Button is off!')
