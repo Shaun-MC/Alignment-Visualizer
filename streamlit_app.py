300a3dec221b4d8d616cd2135131896fd066f603
Shaun-MC <shaunmcushman@gmail.com>
2025-03-03 10:17:41 -0800

    remove button code, store in backup file

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,22 +7,3 @@ st.title("Streamlit Playground")
 dataframe = np.random.randn(10, 20)
 
 st.dataframe(dataframe)
-
-
-# Button Stuff
-""" 
-if 'button' not in st.session_state:
-    st.session_state.button = False
-
-def click_button():
-    st.session_state.button = not st.session_state.button
-
-st.button('Click me', on_click=click_button)
-
-if st.session_state.button:
-    # The message and nested widget will remain on the page
-    st.write('Button is on!')
-    
-else:
-    st.write('Button is off!')
-"""
