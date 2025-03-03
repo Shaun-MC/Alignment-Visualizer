190b2a45b28df40ae0d346bef0e2f5bf6b946b5a
Shaun-MC <shaunmcushman@gmail.com>
2025-03-03 10:02:10 -0800

    more data frame testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,11 @@ import pandas as pd
 st.title("2D Alignment Visualizer")
 st.write("Hello World")
 
-
-
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
-
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 
 if 'button' not in st.session_state:
     st.session_state.button = False
