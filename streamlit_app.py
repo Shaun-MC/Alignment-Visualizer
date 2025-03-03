c1ad6ab523d4f6cde284504971a398b320f5d429
Shaun-MC <shaunmcushman@gmail.com>
2025-03-03 10:24:49 -0800

    using a static table instead of a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ dataframe = pd.DataFrame(
     np.random.randn(10, 20),
     columns=('col %d' % i for i in range(20)))
 
-st.dataframe(dataframe.style.highlight_max(color='blue', axis=0))
+st.table(dataframe.style.highlight_max(color='blue'), axis=0)
