7164630081bf76d68b38803d6fb92dbf09a332df
AaronLouis-error <alaoruoins@gmail.com>
2025-03-10 05:00:37 +0000

    hard coding scoring for demo

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -138,6 +138,27 @@ def runSingleAlignment(input_txt: list) -> str:
             row.append(y_axis[i] + x_axis[j])
         matrix.append(row)
 
+    #todo: calculate scores
+    scores = []
+    row0 = [0,-1,-3,-4,-5,-6,-7,-8]
+    scores.append(row0)
+    row1 = [-1,1,-1,-2,1,0,-1,-2]
+    scores.append(row1)
+    row2 = [-2,5,3,2,1,7,6,5]
+    scores.append(row2)
+    row3 = [-3,4,8,10,9,8,7,13]
+    scores.append(row3)
+    row4 = [-4,3,7,9,11,15,14,13]
+    scores.append(row4)
+    row5 = [-6,1,10,10,9,13,13,17]
+    scores.append(row5)
+    row6 = [-7,0,9,9,15,14,18,17]
+    scores.append(row6)
+    row7 = [-8,-1,8,9,14,21,20,19]
+    scores.append(row7)
+    row8 = [-9,-2,7,15,14,20,20,27]
+    scores.append(row8)
+
     #iterate through the table scoring
     for num in range(len(y_axis) * len(x_axis) + 1):
         # Toggle visibility
@@ -152,7 +173,7 @@ def runSingleAlignment(input_txt: list) -> str:
         # Calculate current row and column positions
         curRow = num // len(x_axis)
         curCol = num % len(x_axis)
-        
+
         # edit table
         table_html = "<table><thead><tr><th></th>"
         for col in range(len(x_axis)):
@@ -161,8 +182,39 @@ def runSingleAlignment(input_txt: list) -> str:
             else:
                 table_html += f"<th> {x_axis[col]} </th>"
         table_html += "</tr></thead><tbody>"
-        
-        # Fill the table cells
+    
+        for row in range(len(y_axis)):
+            if (row == curRow):
+                table_html += f"<tr><td>({y_axis[row]})</td>"
+            else:
+                table_html += f"<tr><td> {y_axis[row]} </td>"
+            for col in range(len(x_axis)):
+                # If this cell should be filled (we've reached it in our iteration)
+                if (row < curRow):
+                    table_html += f"<td>{scores[row][col]}</td>"
+                elif (row <= curRow and col < curCol):
+                    table_html += f"<td>{scores[row][col]}</td>"
+                elif (row == curRow and col == curCol):
+                    table_html += f"<td>{matrix[row][col]}</td>"
+                else:
+                    table_html += "<td></td>"
+            table_html += "</tr>"
+        table_html += "</tbody></table>"
+            
+        table_placeholder.markdown(table_html, unsafe_allow_html=True)
+        time.sleep(1)
+        # todo: this is hard coded
+        # show the scores
+
+        # edit table
+        table_html = "<table><thead><tr><th></th>"
+        for col in range(len(x_axis)):
+            if (col == curCol):
+                table_html += f"<th>({x_axis[col]})</th>"
+            else:
+                table_html += f"<th> {x_axis[col]} </th>"
+        table_html += "</tr></thead><tbody>"
+    
         for row in range(len(y_axis)):
             if (row == curRow):
                 table_html += f"<tr><td>({y_axis[row]})</td>"
@@ -171,17 +223,21 @@ def runSingleAlignment(input_txt: list) -> str:
             for col in range(len(x_axis)):
                 # If this cell should be filled (we've reached it in our iteration)
                 if row < curRow or (row == curRow and col <= curCol):
-                    table_html += f"<td>{matrix[row][col]}</td>"
+                    table_html += f"<td>{scores[row][col]}</td>"
                 else:
                     table_html += "<td></td>"
             table_html += "</tr>"
         table_html += "</tbody></table>"
 
+        table_placeholder.markdown(table_html, unsafe_allow_html=True)
 
-    # Placeholder for the actual alignment logic
-    # For now, just join the input strings with a newline
+   
     #todo: display alignment
-    return "\n".join(input_txt)
+    st.write("Alignment:")
+    st.write("A T C T G A T _ C")
+    st.write("_ T G _ C A T A C")
+    st.write("Score: 27")
+    return "\n"
 
 def runMultipleAlignment(input_txt) -> str:
     return ""
