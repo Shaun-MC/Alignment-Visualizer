b4b011b66806083eec1d85f1007e82ac1c7896f8
Shaun-MC <shaunmcushman@gmail.com>
2025-03-16 11:09:55 -0700

    implemented new 'main' script

---
+++ src/streamlit_app2.py
@@ -0,0 +1,21 @@
+from alignment import Alignment
+import streamlit as st
+
+if __name__ == "__main__":
+
+    # Page Header
+    st.markdown(
+        '<header style="text-align: center"> Sequence Alignment Visualizer </header>', 
+        unsafe_allow_html=True
+    )
+    
+    program = Alignment()
+
+    program.execute()
+
+    # Page Footer
+    st.markdown(
+        '<footer style="text-align: center"> University of Washington Bothell CSSE, (Shaun Cushman, Aaron Gröpper) </footer>', 
+        unsafe_allow_html=True
+    )
+    
