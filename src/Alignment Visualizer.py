87a782fb4d838447b3148b128008a4e7be683509
Shaun-MC <shaunmcushman@gmail.com>
2025-03-17 16:38:32 -0700

    inplemented better header & footer

--- src/Alignment Visualizer.py
+++ src/Alignment Visualizer.py
@@ -3,18 +3,71 @@ import streamlit as st
 
 if __name__ == "__main__":
 
+    header = """
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <title>Custom Header</title>
+        <style>
+            header {
+                background-color: #660066; /* Dark purple background */
+                color: black; /* Text color */
+                text-align: center;
+            padding: 20px;
+            font-family: Arial, sans-serif;
+        }
+        header h1 {
+            margin: 0;
+            font-weight: bold;
+        }
+        header p {
+            margin: 10px 0 0;
+            color: #; 
+        }
+        </style>
+        </head>
+        <body>
+        <header>
+            <h1>AlignView</h1>
+            <p>Customizable Alignment Visualizer</p>
+        </header>
+        </body>
+    """
+
     # Page Header
-    st.markdown(
-        '<header style="text-align: center"> Sequence Alignment Visualizer </header>', 
-        unsafe_allow_html=True
-    )
+    st.markdown(header, unsafe_allow_html=True), 
     
     program = Alignment()
     program.execute()
 
+    footer = """
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Custom Footer</title>
+        <style>
+            footer {
+                background-color: #ddd; /* Light gray background */
+                text-align: center;
+                padding: 10px;
+                font-family: Arial, sans-serif;
+                font-size: 14px;
+                margin-top: 100px;
+            }
+            footer a {
+                color: blue; /* Default link color */
+                text-decoration: none;
+            }
+            footer a:hover {
+                text-decoration: underline;
+            }
+        </style>
+        </head>
+        <body>
+
+        <footer>
+            <p>University of Washington Bothell CSSE</p>
+            <p>This project was created by Shaun Cushman and Aaron Gröpper</p>
+        </footer>
+        </body>
+    """
+
     # Page Footer
-    st.markdown(
-        '<footer style="text-align: center"> University of Washington Bothell CSSE, (Shaun Cushman, Aaron Gröpper) </footer>', 
-        unsafe_allow_html=True
-    )
+    st.markdown(footer, unsafe_allow_html=True)
     
