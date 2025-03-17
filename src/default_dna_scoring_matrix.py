315b3f0f90eb6b8d62aab2ec0dc231a79ec7ccf4
AaronLouis-error <alaoruoins@gmail.com>
2025-03-17 22:38:58 +0000

    no scoring matrix necesarry for dna

---
+++ src/default_dna_scoring_matrix.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+class DefaultDNAScoringMatrix:
+    # todo either mirror input on diagonal axis or make a symetrical 
+    def __init__(self):
+        self.scoring_matrix = None
+        self.total_characters = 5
+        self.scoring_matrix = None
+
+        # Initialize the matrix
+        st.header("Custom Scoring Matrix")
+        labels = ["_", "A", "G", "C", "T"]
+        # Hard-coded data for the scoring matrix
+        data = {
+            "_": [0, 0, 0, 0, 0],
+            "A": [0, 1, 0, 0, 0],
+            "G": [0, 0, 1, 0, 0],
+            "C": [0, 0, 0, 1, 0],
+            "T": [0, 0, 0, 0, 1]
+        }
+
+        # Display the matrix
+        self.scoring_matrix = pd.DataFrame(data, index=labels)
--- src/scoring_matrix_factory.py
+++ src/scoring_matrix_factory.py
@@ -1,6 +1,7 @@
 from dna_scoring_matrix import DNAScoringMatrix
 from rna_scoring_matrix import RNAScoringMatrix
 from protein_scoring_matrix import ProteinScoringMatrix
+from default_dna_scoring_matrix import DefaultDNAScoringMatrix
 
 class ScoringMatrixFactory:
     @staticmethod
@@ -8,7 +9,17 @@ class ScoringMatrixFactory:
 
         if using_scoring_matrix_option == "No":
             # todo: return default scoring matrix for DNA, RNA and Protein
-            return None
+            match sequence_type_option:
+                case "DNA":
+                    return DefaultDNAScoringMatrix()
+                case "RNA":
+                    # todo: make defautl rna matrix
+                    return RNAScoringMatrix()
+                case "Protein":
+                    # todo: make defautl protein matrix
+                    return ProteinScoringMatrix()
+                case _:
+                    raise ValueError("Invalid sequence type option")
         
         match sequence_type_option:
             case "DNA":
