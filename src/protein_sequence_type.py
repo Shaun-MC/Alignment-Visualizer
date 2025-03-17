68f696d9a515a604d91702a015ebb6bcc5f86477
Shaun-MC <shaunmcushman@gmail.com>
2025-03-16 18:51:44 -0700

    updated sequence encoding validation

--- src/protein_sequence_type.py
+++ src/protein_sequence_type.py
@@ -4,7 +4,7 @@ class Protein:
     def __init__(self):
 
         @st.cache_data(persist="disk")
-        def retrieve_valid_nucleotides():
+        def retrieve_valid_nucleotides() -> dict:
             return {
                 'A': None, 'C': None, 'D': None, 'E': None, 'F': None, 'G': None, 'H': None, 'I': None, 'K': None, 
                 'L': None, 'M': None, 'P': None, 'Q': None, 'R': None, 'S': None, 'T': None, 'V': None, 'Y': None
@@ -13,20 +13,23 @@ class Protein:
         self.valid_nucleotides = retrieve_valid_nucleotides()
         self.validated_sequences = list()
 
-    def get_validated_sequences(self):
+    def get_validated_sequences(self) -> list[str]:
         return self.validated_sequences
     
-    def validate_sequences(self, sequences):
+    def validate_encoding(self, sequences: list[str]) -> None:
 
         if sequences is None:
             return
 
-        for sequence in sequences:
-
-            for nucleotide in sequence:
-
+        for i, sequence in enumerate(sequences):
+            
+            upper_sequence = sequence.upper()
+    
+            sequences[i] = upper_sequence
+    
+            for nucleotide in upper_sequence:
                 if nucleotide not in self.valid_nucleotides:
-                    st.write(f":red[Invalid Input Format, Doesn't Meet DNA Standards: ]")
-                    return
+                    st.write(f":red[Invalid Input Format, Doesn't Meet Amino Acid Encoding Scheme (A, C, D, E, F, etc.,): ]")
+                    return 
 
-            self.validated_sequences.append(sequence)
+            self.validated_sequences.append(upper_sequence)
--- src/rna_sequence_type.py
+++ src/rna_sequence_type.py
@@ -10,20 +10,23 @@ class RNA:
         self.valid_nucleotides = retrieve_valid_nucleotides()
         self.validated_sequences = list()
 
-    def get_validated_sequences(self):
+    def get_validated_sequences(self) -> list[str]:
         return self.validated_sequences
     
-    def validate_sequences(self, sequences):
+    def validate_encoding(self, sequences: list[str]) -> None:
 
         if sequences is None:
             return
 
-        for sequence in sequences:
-
-            for nucleotide in sequence:
-
+        for i, sequence in enumerate(sequences):
+            
+            upper_sequence = sequence.upper()
+    
+            sequences[i] = upper_sequence
+    
+            for nucleotide in upper_sequence:
                 if nucleotide not in self.valid_nucleotides:
-                    st.write(f":red[Invalid Input Format, Doesn't Meet DNA Standards: ]")
-                    return
+                    st.write(f":red[Invalid Input Format, Doesn't Match RNA Encoding Scheme (A, C, G, U): ]")
+                    return 
 
-            self.validated_sequences.append(sequence)
+            self.validated_sequences.append(upper_sequence)
