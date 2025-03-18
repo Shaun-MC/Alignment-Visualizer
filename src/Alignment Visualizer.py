1c8575977661b4305f283053679f4a5cd3eb500e
Shaun-MC <shaunmcushman@gmail.com>
2025-03-18 12:45:33 -0700

    improved header and footer modularization

--- src/Alignment Visualizer.py
+++ src/Alignment Visualizer.py
@@ -1,77 +1,16 @@
 from alignment import Alignment
 import streamlit as st
+from header import Header
+from footer import Footer
 
 if __name__ == "__main__":
 
-    header = """
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title>Custom Header</title>
-        <style>
-            header {
-                background-color: #660066; /* Dark purple background */
-                color: black; /* Text color */
-                text-align: center;
-            padding: 20px;
-            font-family: Arial, sans-serif;
-            border-radius: 15px; /* Rounded edges */
-        }
-        header h1 {
-            margin: 0;
-            font-weight: bold;
-        }
-        header p {
-            margin: 10px 0 0;
-            color: #; 
-        }
-        </style>
-        </head>
-        <body>
-        <header>
-            <h1>AlignView</h1>
-            <p>Customizable Alignment Visualizer</p>
-        </header>
-        </body>
-    """
-
     # Page Header
-    st.markdown(header, unsafe_allow_html=True) 
-    
+    Header.display_header() 
+
+    # Execute Algorithm
     program = Alignment()
     program.execute()
 
-    footer = """
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Custom Footer</title>
-        <style>
-            footer {
-                background-color: #660066; /* Dark purple background */
-                text-align: center;
-                padding: 10px;
-                font-family: Arial;
-                font-size: 14px;
-                margin-top: 100px;
-                border-radius: 15px; /* Rounded edges */
-                line-height: 1.5; /* Line height for even spacing between lines */
-            }
-            footer a {
-                color: blue; /* Default link color */
-                text-decoration: none;
-            }
-            footer a:hover {
-                text-decoration: underline;
-            }
-        </style>
-        </head>
-        <body>
-
-        <footer>
-            <p></p>
-            <p>University of Washington Bothell CSSE</p>
-            <p>This project was created by Shaun Cushman and Aaron Gröpper</p>
-        </footer>
-        </body>
-    """
-
     # Page Footer
-    st.markdown(footer, unsafe_allow_html=True)
-    
+    Footer.display_footer()
