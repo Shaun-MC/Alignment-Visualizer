053d85e231dbcfcce61c207923276201d3878de1
Shaun-MC <shaunmcushman@gmail.com>
2026-07-09 20:48:23 +0000

    removed stale imports

--- src/Alignment Visualizer.py
+++ src/Alignment Visualizer.py
@@ -1,11 +1,18 @@
-from alignment.core import Alignment
-from ui.header import Header
 from ui.footer import Footer
+from ui.header import Header
+from alignment.core import Alignment
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
 
 if __name__ == "__main__":
 
     # Page Header
-    Header.display_header() 
+    Header.display_header()
 
     # Execute Algorithm
     program = Alignment()
